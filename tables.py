faaf2fb408459dd1f366a8f1bf0487eb7906e08c
cabito <teban.jar@gmail.com>
2022-08-06 12:32:55 -0500

    primer commit con conversion de archivos pdf a jpg

---
+++ tables.py
@@ -0,0 +1,11 @@
+from sklear.cluster import AgglomerativeClustering
+from pytesseract import Output
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
+import pytesseract
+import argparse
+import imutils
+import cv2
+
+
